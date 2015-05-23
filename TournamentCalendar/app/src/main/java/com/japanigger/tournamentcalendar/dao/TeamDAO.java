1b2d55d922fed2e609d54b77a459e63495757fc7
Cesar <cesar.cozb@gmail.com>
2015-05-23 05:36:10 -0500

    get teams rest

--- TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/dao/TeamDAO.java
+++ TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/dao/TeamDAO.java
@@ -1,20 +1,49 @@
 package com.japanigger.tournamentcalendar.dao;
 
+import android.util.Log;
+
+import com.japanigger.tournamentcalendar.dao.rest.JsonParserUtil;
+import com.japanigger.tournamentcalendar.dao.rest.RestConstants;
 import com.japanigger.tournamentcalendar.data.Team;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by usuario on 5/23/2015.
  */
-public class TeamDAO{
+public class TeamDAO {
 
-       public List<Team> getAll(){
-           List<Team> list = new ArrayList<Team>();
-           list.add(new Team("Argentina"));
-           list.add(new Team("Paraguay"));
-           list.add(new Team("Peru"));
-           return list;
+    public List<Team> getAll() {
+        List<Team> teams = new ArrayList<Team>();
+
+        String resultStr;
+        HttpClient client = new DefaultHttpClient();
+        JsonParserUtil parser = new JsonParserUtil();
+        HttpGet get = new HttpGet(RestConstants.GET_TEAMS);
+        get.setHeader("Accept", "application/json");
+
+        try {
+            HttpResponse httpResponse = client.execute(get);
+            resultStr = EntityUtils.toString(httpResponse.getEntity());
+            Log.d("RESPONSE:: ", "response: " + resultStr);
+            JSONArray jsonTeams = new JSONArray(resultStr);
+            teams = parser.getTeams(jsonTeams);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            Log.e(getClass().getName(), "error parsing team object", e);
         }
+
+        return teams;
+    }
 }
--- TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/data/Team.java
+++ TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/data/Team.java
@@ -1,17 +1,31 @@
 package com.japanigger.tournamentcalendar.data;
 
+import java.util.List;
+
 /**
  * Created by usuario on 5/21/2015.
  */
 public class Team {
 
-    public Team(String name) {
-        this.name = name;
-    }
-
+    private List<TeamPlayer> players;
+    private int id;
     private String name;
 
+    public List<TeamPlayer> getPlayers() {
+        return players;
+    }
 
+    public void setPlayers(List<TeamPlayer> players) {
+        this.players = players;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -19,5 +33,20 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Team(List<TeamPlayer> players, int id, String name) {
+        super();
+        this.players = players;
+        this.id = id;
+        this.name = name;
+    }
+
+    public Team() {
+        super();
+    }
+
+    public Team (String name){
+        this.name=name;
     }
 }
