a089e934a855408f680722ffb0bb9e4b0ce88b60
joseph-higaki <josephhigaki@hotmail.com>
2015-05-23 04:53:24 -0500

    Team class with players List

--- TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/data/Team.java
+++ TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/data/Team.java
@@ -1,17 +1,22 @@
 package com.japanigger.tournamentcalendar.data;
 
+import java.util.List;
+
 /**
  * Created by usuario on 5/21/2015.
  */
 public class Team {
-
-    public Team(String name) {
-        this.name = name;
-    }
-
+    private int id;
     private String name;
+    private List<TeamPlayer> players;
 
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -21,8 +26,33 @@ public class Team {
         this.name = name;
     }
 
+    public Team(List<TeamPlayer> players, int id, String name) {
+        super();
+        this.players = players;
+        this.id = id;
+        this.name = name;
+    }
+    public Team() {
+        super();
+    }
+
+    public Team(String name) {
+        super();
+        this.name = name;
+    }
+
+    public List<TeamPlayer> getPlayers() {
+        return players;
+    }
+    public void setPlayers(List<TeamPlayer> players) {
+        this.players = players;
+    }
+
     @Override
     public String toString() {
         return getName();
     }
+
+
+
 }
