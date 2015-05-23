61b354268626ff82be55dbc403e0e8da217390a7
thenoro <raul.yamashiro@gmail.com>
2015-05-23 13:10:00 -0500

    new match fragment dialog

--- TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/NewMatchDialog.java
+++ TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/NewMatchDialog.java
@@ -9,7 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.japanigger.tournamentcalendar.dao.CityDAO;
+import com.japanigger.tournamentcalendar.data.City;
+
+import java.util.List;
+
 public class NewMatchDialog extends DialogFragment {
+    CityDAO cityDAO;
+    List<City> cities;
+
     Button btnSelectDate;
     int year_x, month_x, day_x;
     static final int DIALOG_ID = 0;
@@ -17,6 +25,9 @@ public class NewMatchDialog extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        cityDAO = new CityDAO(getActivity());
+        cities = cityDAO.getAll();
+
         return inflater.inflate(R.layout.fragment_new_match,null);
     }
 
